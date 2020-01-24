16f5f282ea3df2793662df19fbf830ca7f589571
Dan Herrera <sonarius@shew.io>
2020-01-24 11:47:31 -0500

    Implement login command up to authentication

--- cmd/login.go
+++ cmd/login.go
@@ -1,6 +1,10 @@
 package cmd
 
 import (
+	"net/url"
+
+	"github.com/loksonarius/gli/cfg"
+
 	"github.com/spf13/cobra"
 )
 
@@ -12,7 +16,7 @@ var (
 	endpoint string
 	authType string
 	token    string
-	user     string
+	username string
 	password string
 
 	loginCmd = &cobra.Command{
@@ -27,6 +31,45 @@ along with the given credentials. List these using 'gli target list'.`,
 		Args: cobra.ExactArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
 			logger.Println("Unimplemented")
+
+			// Should panic out if anything's blatantly wrong
+			validateFlags()
+
+			var auth cfg.AuthConfig
+			switch authType {
+			case "basic":
+				auth = cfg.BasicAuthConfig{
+					Endpoint: endpoint,
+					Username: username,
+					Password: password,
+				}
+			case "oauth":
+				auth = cfg.OAuthConfig{
+					Endpoint: endpoint,
+					Token:    token,
+				}
+			case "token":
+				auth = cfg.TokenAuthConfig{
+					Endpoint: endpoint,
+					Token:    token,
+				}
+			}
+
+			client, err := auth.Client()
+			if err != nil {
+				logger.Fatalf(
+					"Received following error creating auth client: %v\n",
+					err,
+				)
+			}
+
+			// Check that credentials actually work with a basic request
+			if _, _, err = client.Users.CurrentUserStatus(); err != nil {
+				logger.Fatalf(
+					"Request failed against user status api: %v\n",
+					err,
+				)
+			}
 		},
 	}
 )
@@ -35,7 +78,7 @@ func init() {
 	loginCmd.Flags().StringVarP(
 		&endpoint,
 		"endpoint",
-		"s",
+		"e",
 		defaultEndpoint,
 		"gitLab endpoint to auth against",
 	)
@@ -57,20 +100,58 @@ func init() {
 	)
 
 	loginCmd.Flags().StringVarP(
-		&user,
-		"user",
+		&username,
+		"username",
 		"u",
 		"",
 		"username to use when auth-type is 'basic'",
 	)
 
 	loginCmd.Flags().StringVarP(
-		&user,
-		"pass",
+		&password,
+		"password",
 		"p",
 		"",
 		"password to use when auth-type is 'basic'",
 	)
 
 	rootCmd.AddCommand(loginCmd)
+}
+
+func validateFlags() {
+	_, err := url.Parse(endpoint)
+	if err != nil {
+		logger.Fatalf(
+			"endpoint '%s' is not a valid URL\n",
+			endpoint,
+		)
+	}
+
+	// ensure auth type is a valid type
+	// ensure flags match with auth type
+	switch authType {
+	case "basic":
+		if username == "" {
+			logger.Fatalln("username must not be empty if using Basic Auth")
+		}
+
+		if password == "" {
+			logger.Fatalln("password must not be empty if using Basic Auth")
+		}
+
+		if token != "" {
+			logger.Fatalln("token only used for token-based auth")
+		}
+	case "oauth":
+	case "token":
+		if token == "" {
+			logger.Fatalln("token must not be empty if using token-based auth")
+		}
+
+		if username != "" || password != "" {
+			logger.Fatalln("username and password only used for Basic Auth")
+		}
+	default:
+		logger.Fatalln("auth-type must one of 'basic', 'oauth', 'token'")
+	}
 }
